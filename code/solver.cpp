519dac4bfa6293d82943106afc62bf7673641f44
AraujoVE <dearaujoviniciuse@gmail.com>
2021-10-16 21:19:44 -0300

    program compilling, yet to test

--- code/solver.cpp
+++ code/solver.cpp
@@ -1,41 +1,204 @@
 #include "solver.hpp"
+SudokuSolver::SudokuSolver(std::vector<std::vector<int>> & sudokuBoard) :
+    SudokuSolver(sudokuBoard, 9, 3){}
 
-SudokuSolver::SudokuSolver(int board_size, int block_size) {
-    if (blockSize*blockSize != boardSize) {
-        printf("[!] ERROR: invalid board_size/block_size\board_size must be equal to block_size² !\n");
-        return;
-    }
-
-    boardSize = board_size; 
+SudokuSolver::SudokuSolver(std::vector<std::vector<int>> & sudokuBoard, int board_size, int block_size) {
+    boardSize = board_size;
     blockSize = block_size;
-    availableValuesRow = std::vector<std::set<int>>(boardSize);
-    availableValuesColumn = std::vector<std::set<int>>(boardSize);
-    availableValuesBlock = std::vector<std::set<int>>(boardSize);
-    board = std::vector<std::vector<int>>(boardSize);
+    int initValue = 0; // all values were not used, so all bits must be 0
+
+    usedValuesRow = std::vector<int>(boardSize, initValue);
+    usedValuesColumn = std::vector<int>(boardSize, initValue);
+    usedValuesBlock = std::vector<int>(boardSize, initValue);
+    board = sudokuBoard;
+    nbOfValuesAvailableToCell = std::vector<std::vector<int>>(boardSize, std::vector<int>(boardSize, 0));
 }
 
-std::vector<int> SudokuSolver::getAvailableValueForCell(int row, int col) {
+void SudokuSolver::initialValuesLookup() {
+    int blockIndex;
+    int lineBlock,colBlock;
+
+    // iterate thought each cell to fill usedValuesRow, usedValuesColumn, usedValuesBlock
+    for (int i = 0; i < boardSize; i++) { 
+        for (int j = 0; j < boardSize; j++) {
+            blockIndex = blockSize * floor((i/blockSize)) + floor((j/blockSize));
+
+            if (board[i][j] != 0) {
+                usedValuesRow[i] += std::pow(2, board[i][j]-1);
+                usedValuesColumn[j] += std::pow(2, board[i][j]-1);
+                usedValuesBlock[blockIndex] += std::pow(2, board[i][j]-1);
+            }
+        }
+    }
+
+    for (int i = 0; i < boardSize; i++) { 
+        for (int j = 0; j < boardSize; j++) {
+            if (board[i][j] != 0)
+                nbOfValuesAvailableToCell[i][j] = (int)getAvailableValuesForCell(i, j).size();
+        }
+    }
+
+    return;
+}
+
+
+//TODO: comment how availableValues work
+std::vector<int> SudokuSolver::getAvailableValuesForCell(int row, int col) {
     int blockIndex = 0;
     blockIndex += blockSize * floor((row/blockSize));
     blockIndex += floor((col/blockSize));
-
+    int curValue;
     std::vector<int> valueFreq(10, 0); // counts the frequencies of 1...9 to see which of them apper in all three sets declared in constructor
-
-    for (int x : availableValuesRow[row])
-        valueFreq[x]++;
-
-    for (int x : availableValuesColumn[col])
-        valueFreq[x]++;
-
-    for (int x : availableValuesBlock[blockIndex])
-        valueFreq[x]++;
-
     std::vector<int> availableValues;
 
-    for (int i = 1; i <= 9; i++) {
-        if (valueFreq[i] == 3) // if the count is 3, the value appers in all sets and can be inserted in cell
-            availableValues.push_back(i);
+    for (int i = 0; i < boardSize; i++) {
+        //For each of the attributes - row, column and block - we verify if the value is in the avaulableValues. 
+        curValue = i+1;
+        valueFreq[curValue] += ((int)std::pow(2,i) & usedValuesRow[row]) ? 0 : 1;
+        valueFreq[curValue] += ((int)std::pow(2,i) & usedValuesColumn[col]) ? 0 : 1;
+        valueFreq[curValue] += ((int)std::pow(2,i) & usedValuesBlock[blockIndex]) ? 0 : 1;
+        if(valueFreq[curValue] == 3) availableValues.push_back(curValue);
+    }
+    
+    return availableValues;
+}
+
+bool SudokuSolver::backtrackingSearch(std::vector<std::vector<bool>> & visited) {
+    bool isBoardCompleted = true;
+    bool toBreak = false;
+
+    // Checking if we have a completed board
+    for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
+            if (board[i][j] == 0) {
+                isBoardCompleted = false;
+                toBreak = true;
+                break;
+            }
+            if(toBreak) break;
+        }
     }
 
-    return availableValues;
+    if (isBoardCompleted) // values are 0, so the board was 100% filled
+        return true;
+
+    if (cellsToVisit.empty())
+        return false;
+    
+    // Visiting cell with max priority (has less options of values to be assigned)
+    int curCellLine = cellsToVisit.top().second.first; // represents the cell row index
+    int curCellCol = cellsToVisit.top().second.second;// represents the cell column index
+    int nbAvailableValues = boardSize - cellsToVisit.top().first; // number of used values, so the available values is boardSize - usedValues
+    cellsToVisit.pop();
+
+    if (visited[curCellLine][curCellCol])
+        return false;
+
+    // mark current cell as visited 
+    visited[curCellLine][curCellCol] = true;
+
+    // backup of priority_queue to do the "rewind" in the backtrack
+    std::priority_queue<std::pair<int, std::pair<int, int>>> cellsToVisit_BACKUP = cellsToVisit;
+
+    // get all possible values to assign to cell
+    std::vector<int> availableValues = getAvailableValuesForCell(curCellLine, curCellCol);
+
+    // propagate information of value assignment to all other empty cells in same row, column and block
+    std::set<std::pair<int, int>> cellsToInsertInPq;
+
+    int blockIndex = blockSize * floor((curCellLine/blockSize)) + floor((curCellCol/blockSize));
+    int lineBlock,colBlock;
+    for (int i = 0; i < boardSize; i++) {
+        if (board[curCellLine][i] == 0) { // LINE
+            nbOfValuesAvailableToCell[curCellLine][i]--;
+            if (!visited[curCellLine][curCellCol])
+                cellsToInsertInPq.insert(std::make_pair(curCellLine, i));
+        }
+
+        if (board[i][curCellCol] == 0) { // COLUMN
+            nbOfValuesAvailableToCell[i][curCellCol]--;
+            if (!visited[curCellLine][curCellCol])
+                cellsToInsertInPq.insert(std::make_pair(i, curCellCol));
+        }
+
+        lineBlock = blockSize*(blockIndex/blockSize) + i/blockSize;
+        colBlock = blockSize*(blockIndex%blockSize) + i%blockSize;
+        if (board[lineBlock][colBlock] == 0) { // BLOCK
+            nbOfValuesAvailableToCell[lineBlock][colBlock]--;
+            if (!visited[curCellLine][curCellCol])
+                cellsToInsertInPq.insert(std::make_pair(lineBlock, colBlock));
+        }  
+
+    }
+
+    // add cells to priority queue with new priority values
+    for (std::pair<int, int> insertCell : cellsToInsertInPq) {
+        int row = insertCell.first;
+        int col = insertCell.second;
+        cellsToVisit.push({nbOfValuesAvailableToCell[row][col], {row, col}});
+    }
+
+    for (int valueToTry : availableValues) {
+        // decide the value to assign to current cell
+        board[curCellLine][curCellCol] = valueToTry;
+
+        // update values for usedValuesRow, usedValuesCol, usedValuesBlock
+        usedValuesRow[curCellLine] += std::pow(2, valueToTry-1);
+        usedValuesColumn[curCellCol] += std::pow(2, valueToTry-1);
+        usedValuesBlock[blockIndex] += std::pow(2, valueToTry-1); 
+        
+        // go deeper in our decision tree (board states), visiting another tree -> DFS
+        isBoardCompleted = backtrackingSearch(visited);
+        if(isBoardCompleted) // if we completed the board, the challenge is over, we can stop the search
+            return true;
+        // undo updates in usedValuesRow, usedValuesCol, usedValuesBlock
+        usedValuesRow[curCellLine] -= std::pow(2, valueToTry-1);
+        usedValuesColumn[curCellCol] -= std::pow(2, valueToTry-1);
+        usedValuesBlock[blockIndex] -= std::pow(2, valueToTry-1); 
+    }
+
+    // starting "backtracking rewind"...
+
+    // if (!isBoardCompleted)... (if we reach here, the board won't be complete)
+    // ...undo propagation of value assignment to all other empty cells in same row, column and bloc
+    for (int i = 0; i < boardSize; i++) {
+        if (board[curCellLine][i] == 0) { // LINE
+            nbOfValuesAvailableToCell[curCellLine][i]++;
+        }
+
+        if (board[i][curCellCol] == 0) { // COLUMN
+            nbOfValuesAvailableToCell[i][curCellCol]++;
+        }
+
+        lineBlock = blockSize*(blockIndex/blockSize) + i/blockSize;
+        colBlock = blockSize*(blockIndex%blockSize) + i%blockSize;
+        if (board[lineBlock][colBlock] == 0) { // BLOCK
+            nbOfValuesAvailableToCell[lineBlock][colBlock]++;
+        }
+    }
+
+    // resets priority_queue
+    cellsToVisit = cellsToVisit_BACKUP;
+
+    // backtracking after DFS
+    visited[curCellLine][curCellCol] = false;
+
+    // refill priority_queue...
+    for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
+            if (board[i][j] == 0 && !visited[i][j]) {
+                cellsToVisit.push({nbOfValuesAvailableToCell[i][j], {i, j}});
+            }
+        }
+    }
+
+    return false;
+}
+
+void SudokuSolver::heuristicSearch() {
+    std::vector<std::vector<bool>> visited(blockSize, std::vector<bool>(blockSize, false));
+ 
+    backtrackingSearch(visited);
+    
+    return;
 }
--- code/solver.hpp
+++ code/solver.hpp
@@ -5,19 +5,29 @@
 #include <vector>
 #include <set>
 #include <cmath>
+#include <queue>
+#include <numeric>
+#include <bitset>
 
 class SudokuSolver {
     private:
         int boardSize; // 9 by 9 board is the most commom one -> boardSize = 9
         int blockSize; // usually, blocks are 3 by 3 in a 9x9 board -> blockSize = 3
-        std::vector<std::set<int>> availableValuesRow; // available values (can be assigned to cells) for each row in the board
-        std::vector<std::set<int>> availableValuesColumn; // available values (can be assigned to cells) for each column in the board
-        std::vector<std::set<int>> availableValuesBlock; // available values (can be assigned to cells) for each block in the board
+        std::vector<int> usedValuesRow; // already used values (can't be assigned to cells) for each row in the board
+        std::vector<int> usedValuesColumn; // already used values (can't be assigned to cells) for each column in the board
+        std::vector<int> usedValuesBlock; // already used values (can't be assigned to cells) for each block in the board
         std::vector<std::vector<int>> board; // sudoku board -> empty cells are marked as 0
+        std::vector<std::vector<int>> nbOfValuesAvailableToCell; // contains the amount of values that can be inserted at each cell
+        std::priority_queue<std::pair<int, std::pair<int, int>>> cellsToVisit; // priority_queue used to determine which will be the next visited cell, this cell will be assigned a value -> the "pair<pair>" is this (nbUsedValuesForCell, (cell_line, cell_column))
 
     public:
-        SudokuSolver(int boardSize, int blockSize); // constructor
-        std::vector<int> getAvailableValueForCell(int row, int col); // returns an array with all the available values that can be assgned to a cell
+        SudokuSolver(std::vector<std::vector<int>> & sudokuBoard);
+        SudokuSolver(std::vector<std::vector<int>> & sudokuBoard, int boardSize, int blockSize); // constructor
+        std::vector<int> getAvailableValuesForCell(int row, int col); // returns an array with all the available values that can be assgned to a cell
+        void heuristicSearch();
+        bool backtrackingSearch(std::vector<std::vector<bool>> & visited);
+        void initialValuesLookup();
+
 };
 
 #endif
